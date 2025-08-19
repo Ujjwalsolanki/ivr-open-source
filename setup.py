5feacec378cdeccac9e65c8c13285ce4605f072a
ujjwalsolanki <ujjwal.programmer@gmail.com>
2025-08-19 10:43:30 -0400

    added requirements and setup files

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ivr-local',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[
+        # Your project dependencies will go here
+        # For now, we'll keep them in requirements.txt
+    ],
+)
